a64ffa4dcb67c8f5bebb243b8ff3ab93eba03fd3
Valentina <chrnoeva@magora.systems>
2016-04-04 09:59:11 +0600

    create project

---
+++ TransitionControllers/TransitionControllers/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  TransitionControllers
+//
+//  Created by Valentina Chernoeva on 04.04.16.
+//  Copyright © 2016 Valentina Chernoeva. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+@end
