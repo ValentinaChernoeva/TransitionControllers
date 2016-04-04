f5c06e7fe9ebc177fa2f117d5455d8265c8fb90a
Valentina <chrnoeva@magora.systems>
2016-04-04 10:04:23 +0600

    create utilits

---
+++ TransitionControllers/TransitionControllers/Utilits.h
@@ -0,0 +1,15 @@
+//
+//  Utilits.h
+//  TransitionTest
+//
+//  Created by Valentina Chernoeva on 25.03.16.
+//  Copyright © 2016 Valentina Chernoeva. All rights reserved.
+//
+
+#ifndef Utilits_h
+#define Utilits_h
+
+#define PERFORM_BLOCK_IF_NOT_NIL(block, ...) if (block != nil) { block(__VA_ARGS__); }
+
+#endif /* Utilits_h */
+
